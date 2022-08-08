d0ef6ac28347dbf55a0d99d0297a3b500c1aa0a5
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-08-08 08:12:27 -0300

    somente usuario do post pode editar e deletar, tirando a / no fim da url

--- theblog/urls.py
+++ theblog/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
   path('', views.HomeView.as_view(), name='home'),
-  path('post/<int:pk>/', views.PostDetail.as_view(), name='detail_post'),
-  path('new_post/', views.PostCreate.as_view(), name='new_post'),
-  path('edit_post/<int:pk>/', views.PostUpdate.as_view(), name='update_post'),
+  path('post/<int:pk>', views.PostDetail.as_view(), name='detail_post'),
+  path('new_post', views.PostCreate.as_view(), name='new_post'),
+  path('edit_post/<int:pk>', views.PostUpdate.as_view(), name='update_post'),
   path('delete_post/<int:pk>', views.PostDelete.as_view(), name='delete_post'),
-  path('new_category/', views.CategoryCreate.as_view(), name='new_category'),
-  path('category/<str:category>/', views.CategoryView, name='category'),
+  path('new_category', views.CategoryCreate.as_view(), name='new_category'),
+  path('category/<str:category>', views.CategoryView, name='category'),
 ]
