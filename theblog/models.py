fbdc342c4551ae1662a6c0496e710e85893652b8
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-08-09 08:15:23 -0300

    add ckeditor

--- theblog/models.py
+++ theblog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse, reverse_lazy
+from ckeditor.fields import RichTextField
 
 from datetime import datetime, date
 
@@ -23,7 +24,8 @@ class Post(models.Model):
   title = models.CharField(max_length=255)
   title_tag = models.CharField(max_length=255, default="Blog Post")
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-  body = models.TextField()
+  body = RichTextField(blank=True, null=True)
+  #body = models.TextField()
   create = models.DateField(auto_now_add=True)
   category = models.CharField(max_length=255, default='coding')
   likes = models.ManyToManyField(User, related_name='blog_posts')
