e911fa575e35834c649f38c0f6a92bcd1bcab654
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-08-12 09:25:09 -0300

    add comments

--- theblog/models.py
+++ theblog/models.py
@@ -43,6 +43,15 @@ class Post(models.Model):
   def get_absolute_url(self):
     return reverse('detail_post', args=[str(self.id)])
 
+class Comment(models.Model):
+  post_comment = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  name = models.CharField(max_length=255)
+  body = models.TextField()
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return "{} - {}".format(self.post_comment, self.name)
+
 class Profile(models.Model):
   user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
   bio = models.TextField()
